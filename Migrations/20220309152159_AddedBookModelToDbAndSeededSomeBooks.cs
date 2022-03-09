403a3a777f603869b7e130b7df50dad8b02bceb4
mahmmoudkinawy <mahmmoudkinawy@gmail.com>
2022-03-09 19:04:10 +0200

    Fixed the bug

---
+++ Migrations/20220309152159_AddedBookModelToDbAndSeededSomeBooks.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BookListRazor.Migrations
+{
+    public partial class AddedBookModelToDbAndSeededSomeBooks : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Book",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Author = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Book", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Book",
+                columns: new[] { "Id", "Author", "Name" },
+                values: new object[] { 1, "Kinawy", "C# 10" });
+
+            migrationBuilder.InsertData(
+                table: "Book",
+                columns: new[] { "Id", "Author", "Name" },
+                values: new object[] { 2, "Kino", "Asp.net Core" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Book");
+        }
+    }
+}
---
+++ Model/ApplicationDbContext.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookListRazor.Model
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Book> Book { get; set; }
+
+        //Here I'm seeding some data
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Book>().HasData(
+                new Book
+                {
+                    Id = 1,
+                    Author = "Kinawy",
+                    Name = "C# 10"
+                },
+                new Book
+                {
+                    Id = 2,
+                    Author = "Kino",
+                    Name = "Asp.net Core"
+                });
+
+        }
+    }
+}
